67673f18b121524232ef86009df3805665cf334f
Kevin <110409671+PrimarisEquilibrium@users.noreply.github.com>
2025-01-01 21:03:41 -0500

    refactor: comment Texture.h

--- include/Texture.h
+++ include/Texture.h
@@ -7,14 +7,23 @@
 class Texture
 {
 public:
+    // The ID/handler of the texture.
     unsigned int ID;
+
+    // The texture target (e.g., GL_TEXTURE_2D, GL_TEXTURE_3D)
     GLenum TARGET;
 
     Texture(GLenum target, GLenum textureUnit);
 
-    /* Loads the texture from file */
+    /**
+     * Loads a texture from a file.
+     *
+     * @param filename The path to the texture file to be loaded.
+     * @param format The texture format of the input file (e.g., GL_RGB, GL_RGBA).
+     */
     void load(const char* filename, GLenum format) const;
-    /* Binds this texture to the target state */
+
+    // Binds this texture to the target state
     void bind() const;
 };
 
